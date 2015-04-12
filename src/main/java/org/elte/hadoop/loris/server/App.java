22e4f37ff5d2c6f72843fea5a25d7619042dcd82
Andras Makoviczki <makoviczki.andras@gmail.com>
2015-04-12 20:12:38 +0200

    Initialize

---
+++ src/main/java/org/elte/hadoop/loris/server/App.java
@@ -0,0 +1,13 @@
+package org.elte.hadoop.loris.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
