53d416d96a3ac1e2b141cb84cb1e95336c5175d5
Andras Makoviczki <makoviczki.andras@gmail.com>
2015-04-13 21:30:15 +0200

    debug

--- src/main/java/org/elte/hadoop/loris/server/services/HDFSAccess.java
+++ src/main/java/org/elte/hadoop/loris/server/services/HDFSAccess.java
@@ -10,7 +10,7 @@ import org.apache.hadoop.security.UserGroupInformation;
 import org.apache.log4j.Logger;
 
 public class HDFSAccess {
-	Logger logger = Logger.getLogger(HDFSAccess.class);
+	//Logger logger = Logger.getLogger(HDFSAccess.class);
 
 	public HDFSAccess() {
 		String myHadoopIp = new String("172.31.14.253");
