a17349ea040281932d227d1d6bc0ec41da665af6
Andras Makoviczki <makoviczki.andras@gmail.com>
2015-04-13 20:53:51 +0200

    set ip

--- src/main/java/org/elte/hadoop/loris/server/services/HadoopFile.java
+++ src/main/java/org/elte/hadoop/loris/server/services/HadoopFile.java
@@ -23,14 +23,14 @@ public class HadoopFile {
 			Configuration conf = new Configuration();
 			Job.getInstance(conf);
 			conf.set("hadoop.job.ugi", "ubuntu");
-			conf.set("fs.defaultFS", "hdfs://localhost:8020");
+			conf.set("fs.defaultFS", "hdfs://172.31.14.253:8020");
 			
 			FileSystem fs = FileSystem.get(conf);
 			
 			//list
 			RemoteIterator<LocatedFileStatus> fileStatusListIterator = fs
 					.listFiles(new Path(
-							"hdfs://localhost:8020/user/ubuntu"),
+							"hdfs://172.31.14.253:8020/user/ubuntu"),
 							true);
 			
 			while (fileStatusListIterator.hasNext()) {
